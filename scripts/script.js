7559b22cd6a6e2bd6588d445d2c0afa5fa077267
Kestutis Kurienius <kurkestutis@gmail.com>
2021-12-13 17:25:12 +0200

    Some changes in header semantic and style.

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,6 @@
-const shuttersID = document.getElementById("intro-shuters");
-const bottom = document.querySelector(".bottom");
+// const shuttersID = document.getElementById("intro-shuters");
+// const bottom = document.querySelector(".bottom");
 
-setTimeout(() => {
-  bottom.classList.add("blue");
-}, 1500);
-
-setTimeout(() => {
-  bottom.classList.add("blue");
-  bottom.remove();
-}, 2700);
-
-setTimeout(() => {
-  shuttersID.remove();
-}, 3500);
+// setTimeout(() => {
+//   bottom.classList.add();
+// }, 1500);
